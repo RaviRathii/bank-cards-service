d5776772b3394151f8b131fb547ad45f8280ec3a
Ravi Rathi <ravirathi@Ravis-MacBook-Air.local>
2025-04-27 15:51:49 +0530

    Setup

---
+++ src/test/java/com/BankingService/Cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BankingService.Cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
